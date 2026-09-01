db0201d3f74ff958c751199bf01cbad11f5530b8
Caio <c@bol.tz>
2026-09-01 00:01:17 -0400

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
+import { inject } from '@vercel/analytics';
 import { render } from 'solid-js/web';
 import App from './App';
 import './styles.css';
+
+inject();
 
 render(() => <App />, document.getElementById('root')!);
